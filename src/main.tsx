5392fda60c199795aeac3392493170abaaf36cdc
Ardy <ardy@defi.sucks>
2024-01-18 17:43:59 -0400

    feat: add materail ui

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 import { Web3Modal } from '~/components';
+import { ThemeProvider } from '~/providers';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const reactApp: any = (
   <React.StrictMode>
     <BrowserRouter>
-      <Web3Modal>
-        <App />
-      </Web3Modal>
+      <ThemeProvider>
+        <Web3Modal>
+          <App />
+        </Web3Modal>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
