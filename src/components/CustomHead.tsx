9823faffb62820ca11da062dbb35348e773c248d
Ardy <ardy@defi.sucks>
2024-01-26 13:18:20 -0400

    feat: add open graph title

--- src/components/CustomHead.tsx
+++ src/components/CustomHead.tsx
@@ -10,7 +10,20 @@ interface MetadataProps {
 export const CustomHead = ({ title }: MetadataProps) => {
   return (
     <Head>
+      {/* temporary values */}
       <title>{`${title} - Superchain Bridge`}</title>
+      <link rel='icon' href='/favicon.ico' type='image/x-icon' sizes='48x48' />
+
+      <meta property='og:title' content={`${title} - Superchain Bridge`} />
+
+      <meta name='twitter:image' content={'https://defi.sucks/share.jpg'} />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:site' content='@DeFi_Wonderland' />
+      <meta name='twitter:creator' content='@DeFi_Wonderland' />
+      <meta name='twitter:title' content={`${title} - Superchain Bridge`} />
+      {/* <meta name='twitter:description' content={descriptionText} /> */}
+      <meta name='twitter:image' content={'https://defi.sucks/share.jpg'} />
     </Head>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useChainId } from 'wagmi';
 
 import Landing from './landing';
+import { useChain } from '~/hooks';
+import { replaceSpacesWithHyphens } from '~/utils';
 
 const Home = () => {
-  const chainId = useChainId();
+  const { fromChain } = useChain();
   const router = useRouter();
 
+  // Update the URL to reflect the 'From' chain
   useEffect(() => {
-    if (chainId) router.replace({ pathname: `/[chain]`, query: { chain: chainId } });
-  }, [chainId, router]);
+    if (fromChain) router.replace({ pathname: `/[chain]`, query: { chain: replaceSpacesWithHyphens(fromChain.name) } });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fromChain]);
 
   return <Landing />;
 };
