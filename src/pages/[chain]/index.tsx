b533848a67814ce0a4b8785483308fe27a411767
Ardy <ardy@defi.sucks>
2024-01-29 11:48:24 -0400

    fix: supported chains typing on build

--- src/pages/[chain]/index.tsx
+++ src/pages/[chain]/index.tsx
@@ -1,6 +1,7 @@
 import { InferGetStaticPropsType } from 'next';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { Chain as ChainType } from 'viem';
 
 import { CustomHead } from '~/components';
 import { useChain } from '~/hooks';
@@ -23,7 +24,8 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context: { params: { chain: string } }) => {
   const path = context.params?.chain;
-  const title = supportedChains.find((chain) => replaceSpacesWithHyphens(chain.name) === path)?.name || '';
+  const chains = [...supportedChains] as ChainType[]; // Clone the array to avoid mutating the original
+  const title = chains.find((chain: ChainType) => replaceSpacesWithHyphens(chain.name) === path)?.name || '';
 
   return { props: { title } };
 };
