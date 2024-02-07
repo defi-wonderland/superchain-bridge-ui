3ed1f686240c921e8d3309064a46e9fd9e576382
Ardy <ardy@defi.sucks>
2024-02-07 08:15:16 -0400

    chore: update deposit functions

--- src/utils/parsedAbis.ts
+++ src/utils/parsedAbis.ts
@@ -45,6 +45,26 @@ export const finalizeBridgeERC20ABI = parseAbi([
 ]);
 
 /**
+ * @notice Sends ETH to a receiver's address on the other chain. Note that if ETH is sent to a
+ *         smart contract and the call fails, the ETH will be temporarily locked in the
+ *         StandardBridge on the other chain until the call is replayed. If the call cannot be
+ *         replayed with any amount of gas (call always reverts), then the ETH will be
+ *         permanently locked in the StandardBridge on the other chain. ETH will also
+ *         be locked if the receiver is the other bridge, because finalizeBridgeETH will revert
+ *         in that case.
+ *
+ * @param _to          Address of the receiver.
+ * @param _minGasLimit Minimum amount of gas that the bridge can be relayed with.
+ * @param _extraData   Extra data to be sent with the transaction. Note that the recipient will
+ *                     not be triggered with this data, but it will be emitted and can be used
+ *                     to identify the transaction.
+ */
+
+export const bridgeETHToABI = parseAbi([
+  'function bridgeETHTo(address _to, uint32 _minGasLimit, bytes calldata _extraData) external payable',
+]);
+
+/**
  * @custom:legacy
  * @notice Deposits some amount of ERC20 tokens into a target account on L2.
  *
